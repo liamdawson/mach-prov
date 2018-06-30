a7b40a21454377f77095c0f3edee95fe3f220789
Liam Dawson <liam@ldaws.com>
2018-06-30 22:12:36 +1000

    remove reduce statement

--- states/__init__.py
+++ states/__init__.py
@@ -1,11 +1,6 @@
 from . import gnome_shell, apt_get, xps_15
 
-states = reduce(lambda current_states, new_states: current_states + new_states,
-[
-  apt_get.states,
-  xps_15.states,
-  gnome_shell.states
-], [])
+states = [] + apt_get.states + xps_15.states + gnome_shell.states
 
 def relevant_states(tags):
   return filter(lambda state: state.should_apply(tags), states)
