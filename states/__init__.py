7c651f11df86ff2d6b8323b6c985024729e45f44
Liam Dawson <liam@ldaws.com>
2018-07-01 01:38:17 +1000

    link ubuntu states

--- states/__init__.py
+++ states/__init__.py
@@ -1,6 +1,6 @@
-from . import gnome_shell, apt_get, xps_15, user_shell
+from . import gnome_shell, apt_get, xps_15, user_shell, ubuntu
 
-states = [] + apt_get.states + xps_15.states + gnome_shell.states + user_shell.states
+states = [] + apt_get.states + xps_15.states + gnome_shell.states + user_shell.states + ubuntu.states
 
 def relevant_states(tags):
   return filter(lambda state: state.should_apply(tags), states)
