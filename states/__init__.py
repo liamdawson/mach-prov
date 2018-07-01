4d2dc2aa8eabd05c456c1ae22ca7fa1898737afd
Liam Dawson <liam@ldaws.com>
2018-07-01 13:07:11 +1000

    link microsoft states

--- states/__init__.py
+++ states/__init__.py
@@ -1,6 +1,6 @@
-from . import gnome_shell, apt_get, xps_15, user_shell, ubuntu
+from . import gnome_shell, apt_get, xps_15, user_shell, ubuntu, microsoft
 
-states = [] + apt_get.states + xps_15.states + gnome_shell.states + user_shell.states + ubuntu.states
+states = [] + apt_get.states + xps_15.states + gnome_shell.states + user_shell.states + ubuntu.states + microsoft.states
 
 def relevant_states(tags):
   return filter(lambda state: state.should_apply(tags), states)
